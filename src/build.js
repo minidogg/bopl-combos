521fc7ff13b96a6ea503b72f2e67cae99a64f20b
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-05-27 16:23:21 -0500

    time for a lot of fine tuning

--- src/build.js
+++ src/build.js
@@ -32,32 +32,60 @@ const buildHtml = ()=>{
     return combos.join("\n")
 }
 
+var crypto = require('crypto');
+function hash(text){
+    return crypto.createHash('md5').update(text).digest('hex');
+}
+
+const removeExtraDecimals = (num,decimal)=>{
+    return parseFloat(num).toPrecision(Math.round(num).toString().length + decimal);
+}
 const buildArray = (abils)=>{
     const possibleCombos = Object.keys(abils).length**3
     console.log("generating "+possibleCombos+" possible combos and rating them")
 
     let combos = []
+    let pseudoHashes = []
     for(let aa = 0;aa<Object.keys(abils).length;aa++){
         for(let bb = 0;bb<Object.keys(abils).length;bb++){
             for(let cc = 0;cc<Object.keys(abils).length;cc++){
+                let pseudoHash = aa*bb*cc
+                if(pseudoHashes.includes(pseudoHash))continue
+                pseudoHashes.push(pseudoHash)
                 let score = 0;
                 let a = Object.keys(abils)[aa]
                 let b = Object.keys(abils)[bb]
                 let c = Object.keys(abils)[cc]
+                if(a==b||a==c)score-=1
+                if(b==c)score-=1
+
+
                 let types = abils[a].types.concat(abils[b].types).concat(abils[c].types)
                 if(types.includes("movement"))score+=1
                 if(types.includes("defensive"))score+=1
                 if(types.includes("offensive"))score+=1
-                if(types.includes("timestop"))score+=1
+
+                if(abils[a].helps.includes(b) || abils[a].helps.includes(c))score+=1
+                if(abils[b].helps.includes(a) || abils[b].helps.includes(c))score+=1
+                if(abils[c].helps.includes(b) || abils[c].helps.includes(a))score+=1
+
+                if(abils[a].counters.includes(b) || abils[a].counters.includes(c))score-=1
+                if(abils[b].counters.includes(a) || abils[b].counters.includes(c))score-=1
+                if(abils[c].counters.includes(b) || abils[c].counters.includes(a))score-=1
+
+                if(typeof(abils[a].score)!="undefined")score+=abils[a].score
+                if(typeof(abils[b].score)!="undefined")score+=abils[b].score
+                if(typeof(abils[c].score)!="undefined")score+=abils[c].score
 
 
-                combos.push([a,b,c,score])
+
+                combos.push([a,b,c,removeExtraDecimals(score,1)])
             }
         }
     }
     combos.sort((a,b)=>a[3]-b[3]).reverse()
 
-    console.log("done generating "+possibleCombos+" possible combos and rating them")
+    console.log("done generating "+pseudoHashes.length+" possible combos and rating them")
 
     return combos
 }
