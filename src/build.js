59a8d20f60c37f56943cb335a26b7af39675f6aa
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-05-27 18:23:05 -0500

    aaa

--- src/build.js
+++ src/build.js
@@ -8,9 +8,14 @@ module.exports.buildSite = ()=>{
     console.log("building website")
     if(fs.existsSync(generatedPath))fs.rmSync(generatedPath,{recursive:true})
     fs.cpSync(appPath, generatedPath, {recursive: true});
-    let res = buildHtml()
+
+    let cache = JSON.parse(fs.readFileSync("./cached.json","utf-8"))
+    let res = ["",{}]
+    res = buildHtml()
+    fs.writeFileSync("./cached.json",JSON.stringify(res))
+
     let indexHtml = fs.readFileSync(path.join(generatedPath,"/index.html"),"utf-8")
-    indexHtml = indexHtml.replace("%combos%",res)
+    indexHtml = indexHtml.replace("%combos%",res[0])
     fs.writeFileSync(path.join(generatedPath,"/index.html"),indexHtml,"utf-8")
 
     console.log("done building website")
@@ -18,7 +23,7 @@ module.exports.buildSite = ()=>{
 
 const buildHtml = ()=>{
     let combos = buildArray(JSON.parse(fs.readFileSync("./ability.json")))
-    combos = combos.map(combo=>{
+    let combosNew = combos.map(combo=>{
         return `
         <div class="combo">
             <img src="/img/abilities/${combo[0]}.png" alt="${combo[0]}" class="ability">
@@ -29,7 +34,7 @@ const buildHtml = ()=>{
         `
     })
 
-    return combos.join("\n")
+    return [combosNew.join(""),combos]
 }
 
 var crypto = require('crypto');
@@ -37,6 +42,18 @@ function hash(text){
     return crypto.createHash('md5').update(text).digest('hex');
 }
 
+function searchForArray(haystack, needle){
+    var i, j, current;
+    for(i = 0; i < haystack.length; ++i){
+      if(needle.length === haystack[i].length){
+        current = haystack[i];
+        for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
+        if(j === needle.length)
+          return i;
+      }
+    }
+    return -1;
+  }
 const removeExtraDecimals = (num,decimal)=>{
     return parseFloat(num).toPrecision(Math.round(num).toString().length + decimal);
 }
@@ -45,19 +62,15 @@ const buildArray = (abils)=>{
     console.log("generating "+possibleCombos+" possible combos and rating them")
 
     let combos = []
-    let pseudoHashes = []
     for(let aa = 0;aa<Object.keys(abils).length;aa++){
         for(let bb = 0;bb<Object.keys(abils).length;bb++){
             for(let cc = 0;cc<Object.keys(abils).length;cc++){
-                let pseudoHash = aa*bb*cc
-                if(pseudoHashes.includes(pseudoHash))continue
-                pseudoHashes.push(pseudoHash)
                 let score = 0;
                 let a = Object.keys(abils)[aa]
                 let b = Object.keys(abils)[bb]
                 let c = Object.keys(abils)[cc]
-                if(a==b||a==c)score-=1
-                if(b==c)score-=1
+                if(a==b||a==c)score-=1.5
+                if(b==c)score-=1.5
 
 
                 let types = abils[a].types.concat(abils[b].types).concat(abils[c].types)
@@ -78,14 +91,15 @@ const buildArray = (abils)=>{
                 if(typeof(abils[c].score)!="undefined")score+=abils[c].score
 
 
-
-                combos.push([a,b,c,removeExtraDecimals(score,1)])
+                let final = [a,b,c].sort()
+                final.push(removeExtraDecimals(score,1))
+                if(searchForArray(combos,final)==-1)combos.push(final)
             }
         }
     }
     combos.sort((a,b)=>a[3]-b[3]).reverse()
 
-    console.log("done generating "+pseudoHashes.length+" possible combos and rating them")
+    console.log("done generating "+combos.length+" possible combos and rating them")
 
     return combos
 }
--- src/generated/script.js
+++ src/generated/script.js
@@ -1,10 +1,39 @@
-// const exclude = document.getElementById("exclude")
-// exclude.onchange = ()=>{
-//     const combos = Array.from(document.querySelector(".container").children)
-//     combos.forEach(e=>{
-//         e.display = "flex"
-//         exclude.value.split(",").forEach(a=>{
-//             if(e.innerHTML.includes(a))e.display = "none"
-//         })
-//     })
-// }
+const exclude = document.getElementById("exclude")
+const search = document.getElementById("search")
+const combos = Array.from(document.querySelector(".container").children)
+
+
+setInterval(()=>{
+    let excludeArray =
+     exclude.value.split(",")
+    let makeVisible = false
+    if(exclude.value=="")makeVisible = true
+    combos.forEach(e=>{
+        e.style.display = "flex"
+        if(makeVisible==true)return;
+        excludeArray.forEach(a=>{
+            Array.from(e.querySelectorAll("img")).forEach(b=>{
+                if(b.alt.toLowerCase().includes(a.toLowerCase()))e.style.display="none"
+            })
+        })
+    })
+
+    makeVisible = false
+    let searchArray = search.value.split(",")
+    if(search.value=="")makeVisible = true
+    if(makeVisible==false){
+        combos.forEach(e=>{
+            searchArray.forEach(a=>{
+                let show = true
+                Array.from(e.querySelectorAll("img")).forEach(b=>{
+                    if(b.alt.toLowerCase().includes(a.toLowerCase()))show = false
+                })
+                if(show==true)e.style.display="none"
+            })
+        })
+    }
+
+
+    let visible = combos.filter(e=>e.style.display=="flex").length
+    document.getElementById("visibleCount").textContent = "Shown Combos: "+visible
+},500)
